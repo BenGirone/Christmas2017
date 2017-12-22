652338f50a80a21698a69e693c6c5b6bd7c1983f
BenGirone <bgirone001@student.franciscan.edu>
2017-12-22 16:29:02 -0500

    jump animation and road

--- public/anim/collisions.js
+++ public/anim/collisions.js
@@ -2,57 +2,80 @@
 //move the mouse, the sprite responds to overlapings, collision,
 //and displaces another sprite
 
-var box, asterisk, cloud, circle;
+var alyssa;
+var groundLevel = 300;
+var airborne = false;
+var road;
+var roadImg;
+
+function updateAlyssa()
+{
+  if (alyssa.velocity.y != 0)
+      airborne = true;
+
+  if (alyssa.position.y < groundLevel)
+  {
+    alyssa.velocity.y += 1;
+    alyssa.changeAnimation("stretch");
+  }
+  else
+  {
+    alyssa.velocity.y = 0;
+    airborne = false;
+  }
+
+  if (keyDown("w"))
+  {
+    if (alyssa.velocity.y <= 0)
+    {
+      if (alyssa.position.y > 180)
+      {
+        alyssa.velocity.y = -13;
+        alyssa.changeAnimation("stretch");
+      }
+    }
+  }
+  else
+  {
+    if (airborne)
+    {
+      alyssa.changeAnimation("stretch");
+    }
+    else
+    {
+      alyssa.changeAnimation("normal");
+    }
+
+  }
+}
 
 function setup() 
 {
-    var canvas = createCanvas(800, 400);
+  var canvas = createCanvas(900, 400);
 
-    canvas.parent('anim-canvas');
+  canvas.parent('anim-canvas');
 
-    //create 4 sprites
-    alyssa = createSprite(305, 255);
-    alyssa.addAnimation('normal', 'public/anim/assets/alyssa_0000.png', 'public/anim/assets/alyssa_0002.png');
-    alyssa.addAnimation("stretch", 'public/anim/assets/alyssa_0003.png', 'public/anim/assets/alyssa_0003.png')
-    alyssa.position.x = 200;
-    alyssa.position.y = 250;
+
+  roadImg = loadImage("public/anim/assets/rainbow_road_0000.png");
+  road = createSprite(450, 350);
+  road.addImage(roadImg);
+
+  alyssa = createSprite(122, 102);
+  alyssa.addAnimation('normal', 'public/anim/assets/alyssa_0000.png', 'public/anim/assets/alyssa_0002.png');
+  alyssa.addAnimation("stretch", 'public/anim/assets/alyssa_0003.png', 'public/anim/assets/alyssa_0003.png')
+  alyssa.position.x = 100;
+  alyssa.position.y = groundLevel;
+  alyssa.rotateToDirection = true;
+  alyssa.setCollider("rectangle", 0,0,122,102);
 }
 
-function draw() 
+function draw()
 {
-    background(120, 120, 120);
+  background(255,182,193);
 
-    alyssa.debug = mouseIsPressed;
+  alyssa.debug = mouseIsPressed;
 
-    if (alyssa.position.y < 250)
-    {
-        alyssa.velocity.y += 1;
-        alyssa.changeAnimation("stretch");
-    }
-    else
-    {
-      alyssa.velocity.y = 0;
-    }
+  updateAlyssa();
 
-    if (keyDown("w"))
-    {
-        if (alyssa.position.y > 120)
-        {
-          alyssa.changeAnimation("stretch");
-          alyssa.velocity.y = -15;
-        }
-    }
-    else
-    {
-        if (alyssa.position.y < 250)
-        {
-            alyssa.changeAnimation("stretch");
-        }
-        else
-        {
-            alyssa.changeAnimation("normal");
-        }
-    }
-
-    drawSprites();
+  drawSprites();
 }
